26a81404099b66ca994294017117f5b66710e43e
milanshresta <milon.stha88@gmail.com>
2022-11-09 22:04:56 +0545

    corrected the navigation bar

--- product-page/src/components/header/Nav.js
+++ product-page/src/components/header/Nav.js
@@ -1,44 +1,51 @@
 import './Nav.css';
 import logo from '../../Assets/Logo.png';
-import { BsPerson } from 'react-icons/bs';
-import { IconContext } from 'react-icons';
+import { MdPersonOutline } from 'react-icons/md';
+import { BiShoppingBag } from 'react-icons/bi';
 
 export default function Navbar() {
     return (
-        <div className="Nav-bar">
-            <div className='logo'>
-                {/*Always add image using {add the image inside this bracket}*/}
-                <img
-                    src={logo}
-                    alt='website logo'
-                    id='logo'
-                />
+        <>
+            <div className="Nav-bar">
+                <div className='logo'>
+                    {/*Always add image using {add the image inside this bracket}*/}
+                    <img
+                        src={logo}
+                        alt='website logo'
+                        id='logo'
+                    />
+                </div>
+                <div className='nav-content'>
+                    <ul>
+                        <li>
+                            <a href='#'>Collections</a>
+                        </li>
+
+                        <li>
+                            <a href='#'>About</a>
+                        </li>
+
+                        <li>
+                            <a href='#'>Contact</a>
+                        </li>
+
+                        <li>
+                            <a href='#'> <MdPersonOutline
+                                size={"1.4rem"}
+                                className="account-icon"
+                            />Account</a>
+                        </li>
+
+                        <li>
+                            <a href='#'> <BiShoppingBag
+                                size={"1.4rem"}
+                                className="shopping-icon"
+                            />Shopping</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div className='nav-content'>
-                <ul>
-                    <li>
-                        <a href='#'>Collections</a>
-                    </li>
-
-                    <li>
-                        <a href='#'>About</a>
-                    </li>
-
-                    <li>
-                        <a href='#'>Contact</a>
-                    </li>
-
-                    <li>
-                        <a href='#'> <BsPerson 
-                        size={"1.4rem"}
-                        />Account</a>
-                    </li>
-
-                    <li>
-                        <a href='#'>Shoping</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+            <span><hr className='nav-line'/></span>
+        </>
     )
 }
