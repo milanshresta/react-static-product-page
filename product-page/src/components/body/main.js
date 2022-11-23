1d7309133e1731466392f4d398fa4b3d8f12122a
[milanshresta] <[milon.stha88@gmail.com]>
2022-11-23 12:17:06 +0545

    added another product section

--- product-page/src/components/body/main.js
+++ product-page/src/components/body/main.js
@@ -11,33 +11,47 @@ export default function Main() {
             <div className="our-partners">
                 <div className='partner1'>
                     <img
-                    src={Partner1}
-                    alt=''
+                        src={Partner1}
+                        alt=''
                     />
                 </div>
                 <div className='partner2'>
-                <img
-                    src={Partner2}
-                    alt=''
+                    <img
+                        src={Partner2}
+                        alt=''
                     />
                 </div>
                 <div className='partner3'>
-                <img
-                    src={Partner3}
-                    alt=''
+                    <img
+                        src={Partner3}
+                        alt=''
                     />
                 </div>
                 <div className='partner4'>
-                <img
-                    src={Partner4}
-                    alt=''
+                    <img
+                        src={Partner4}
+                        alt=''
                     />
                 </div>
                 <div className='partner5'>
-                <img
-                    src={Partner5}
-                    alt=''
+                    <img
+                        src={Partner5}
+                        alt=''
                     />
+                </div>
+            </div>
+
+            <div className='featured-product'>
+                <div className='product-box product-image'></div>
+                <div className='product-box'>
+                    <div className='product-box1'>
+                        <div className='box1 box-image'></div>
+                        <div className='box1 box-image'></div>
+                    </div>
+                    <div className='product-box2'>
+                        <div className='box2 box-image'></div>
+                        <div className='box2 box-image'></div>
+                    </div>
                 </div>
             </div>
         </>
