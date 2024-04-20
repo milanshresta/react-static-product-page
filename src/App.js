b376ee24b0dc70605e4df2e4c5b8e2fd8b198b79
milansth <milan.shrestha@sworp.com>
2024-04-21 02:10:49 +0545

    user ui update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import './App.css';
-import Navbar from './components/header/Nav';
-import Landingpage from './components/header/Landingpage';
-import Main from './components/body/main';
+import "./App.css";
+import Navigation from "./components/main/Nav"
 
 function App() {
   return (
-    <div className="App">
-
-      <div className='Navbar'>
-        <Navbar />
-      </div>
-      
-      <div className='Landingpage'>
-        <Landingpage />
-      </div>
-      
-      <div className='Main'>
-        <Main />
-      </div>
-
-    </div>
-
+    <Navigation>
+    </Navigation>
   );
 }
 
