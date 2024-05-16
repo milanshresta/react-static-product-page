fbef9706d50222d82fc9b6e51c12ddce71331fd0
milansth <milan.shrestha@sworp.com>
2024-05-16 11:14:24 +0545

    integrate google analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import ReactGA from "react-ga4";
+import useEffect from "react";
 import "./App.css";
 import Header from "./components/main/Header";
 import Footer from "./components/main/Footer";
 
+ReactGA.initialize("Your-Measurement-ID");
+
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("G-NZNQ0SG1YY");
+  }, []);
+  ReactGA.send({ hitType: "pageview", page: window.location.pathname });
   return (
     <div>
       <Header></Header>
